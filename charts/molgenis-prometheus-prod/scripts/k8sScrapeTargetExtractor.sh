6ace226c48578d929dbc0453a7d91741ec6041ae
christianhilbrands <c.g.hilbrands@gmail.com>
2019-10-29 11:01:15 +0100

    test(prometheus-prod) missed script rancher command

--- charts/molgenis-prometheus-prod/scripts/k8sScrapeTargetExtractor.sh
+++ charts/molgenis-prometheus-prod/scripts/k8sScrapeTargetExtractor.sh
@@ -102,7 +102,7 @@ printf "Branch 0.1 - finished\n"
 printf '%s\n' "${outputArray[@]}" >> 01TargetsAcquired.yml
 printf "Writing outputArray to 01TargetsAcquired.yml done\n"
 echo $(rancher kubectl config set-context edgecluster --namespace=molgenis-prometheus-prod)
-echo $(rancher kubectl create configmap targets-configmap --from-file masterTargetsAcquired.yml --from-file 20TargetsAcquired.yml --from-file 10TargetsAcquired.yml --from-file 01TargetsAcquired.yml -o yaml --dry-run | kubectl replace -f -)
+echo $(rancher kubectl create configmap targets-configmap --from-file masterTargetsAcquired.yml --from-file 20TargetsAcquired.yml --from-file 10TargetsAcquired.yml --from-file 01TargetsAcquired.yml -o yaml --dry-run | rancher kubectl replace -f -)
 printf "Rancher kubectl updatet configmap from molgenis-prometheus-prod\n"
 $(rm -f *TargetsAcquired.yml)
 printf "Removed artifacts\n"printf "Script done\n"
