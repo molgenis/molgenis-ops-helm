c303cdb14333ebc4e86336e07d6130e3925c9dbb
christianhilbrands <c.g.hilbrands@gmail.com>
2020-08-20 14:51:57 +0200

    changes: - serverlist: added monitoring to list - serverlist: monitoring{yes|no} - script: public to private list - script: if monitoring == no then not in list - script: changes cars to lower case
    changed:
    - reference to secrets
    
    test secrets
    
    changed cronjob-secrets: data to stringDate
    
    changed: cronjob-secrets: missed conversion
    
    test: cronjob env vars
    
    test: cronjob decoded secrets
    
    changed: typo cronjob
    
    changed: working decryption base64
    
    removed test decrypt base64

--- charts/molgenis-prometheus/scripts/targetWebGet.sh
+++ charts/molgenis-prometheus/scripts/targetWebGet.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "Start script"
 githubtoken=$1
+serverlistuser=$2
+serverlistpassword=$3
 serverlistServer=molgenis23.gcc.rug.nl
 NePort=9100
 serverlist=$(curl -s https://${githubtoken}@raw.githubusercontent.com/molgenis/molgenis-ops-ansible/master/inventory.ini)
@@ -11,8 +13,9 @@ acceptTargetArray=("")
 acceptWebArray=("- targets:")
 prodTargetArray=("")
 prodWebArray=("- targets:")
+serverlisttoken="$(curl -X POST -H "Content-Type: application/json" --data "{\"username\":\"${serverlistuser}\",\"password\":\"${serverlistpassword}\"}" "https://${serverlistServer}/api/v1/login" | cut -d'"' -f4 )"
 echo "Serverlist received"
-echo "Start domain retrieval"
+echo "Start iteration"
 for val in $serverlist;
 do
     if [[ $val =~ ['['] ]];
@@ -30,8 +33,13 @@ do
     elif [[ $val =~ ".gcc.rug.nl" ]] || [[ $val =~ ".target.rug.nl" ]];
     then
         host=$(echo $val | cut -d'.' -f1)
-        desc=$(curl -s https://$serverlistServer'/api/v2/mm_public_serverlist?q=id=='$host'&attrs=~id,description' | rev | cut -d'"' -f2 | rev )
-        result=$(curl -s https://$serverlistServer'/api/v2/mm_public_serverlist?q=id=='$host'&attrs=~id,url,dns' | grep -Eo "(http|https)://[a-zA-Z0-9./?=_-]*\:?\d*/?[a-zA-Z0-9./?=_-]*/?" | uniq | tr '\n' ' ' )
+        monitoring="$(curl -s -H "x-molgenis-token: ${serverlisttoken}" -X GET "https://${serverlistServer}/api/v2/molgenis_serverlist?q=id==${host}&attrs=~id,monitoring" | rev | cut -d'"' -f1 | rev | cut -d':' -f2 | rev | cut -c 4- | rev )"
+        if [[ "$monitoring" =~ "false" ]];
+        then
+            continue
+        fi
+        desc=$(curl -s -H "x-molgenis-token: ${serverlisttoken}" -X GET "https://${serverlistServer}/api/v2/molgenis_serverlist?q=id==$host&attrs=~id,description" | rev | cut -d'"' -f2 | rev )
+        result=$(curl -s -H "x-molgenis-token: ${serverlisttoken}" -X GET "https://${serverlistServer}/api/v2/molgenis_serverlist?q=id==$host&attrs=~id,DNS,DNS_alias" | grep -Eo "(http|https)://[a-zA-Z0-9./?=_-]*\:?\d*/?[a-zA-Z0-9./?=_-]*/?" | uniq | tr '\n' ' ' )
         case $group in 
             "prod")
                 if [[ "$host" == 'gbic' ]]; 
@@ -77,6 +85,8 @@ do
         esac
     fi
 done
+endtoken="$(curl -H "x-molgenis-token: ${serverlisttoken}" -X POST "https://${serverlistServer}/api/v1/logout")"
+echo "Connection to serverlist closed"
 prodWebArray+=("  - https://www.chromosome6.org/contact")
 prodWebArray+=("  - https://github.com/molgenis/systemsgenetics/wiki/Genotype-Harmonizer-Download")
 prodWebArray+=("  - https://github.com/molgenis/systemsgenetics/wiki/Genotype-Harmonizer")
@@ -90,7 +100,7 @@ printf '%s\n' "${acceptWebArray[@]}" >> accept-http-list.yml
 printf '%s\n' "${acceptTargetArray[@]}" >> accept-target-list.yml
 printf '%s\n' "${testWebArray[@]}" >> test-http-list.yml
 printf '%s\n' "${testTargetArray[@]}" >> test-target-list.yml
-echo $(kubectl --token=$TOKEN create configmap targets-configmap --from-file prod-target-list.yml --from-file accept-target-list.yml --from-file test-target-list.yml --from-file prod-http-list.yml --from-file accept-http-list.yml -o yaml --dry-run | kubectl --token=$TOKEN replace -f -)
+echo $(kubectl --token=$token create configmap targets-configmap --from-file prod-target-list.yml --from-file accept-target-list.yml --from-file test-target-list.yml --from-file prod-http-list.yml --from-file accept-http-list.yml -o yaml --dry-run | kubectl --token=$token replace -f -)
 echo $(rm -rf *.yml)
 echo "Done retrieving and saving to file"
 echo "End script"
