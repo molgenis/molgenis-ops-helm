34213fb8b34659e5ed6b160aba9667f7c7ae59d5
Fleur Kelpin <f.kelpin@umcg.nl>
2021-01-07 12:27:22 +0100

    doc: move local test command to shellfile

---
+++ ct.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+command="cd /data && helm init --client-only --stable-repo-url https://charts.helm.sh/stable && "
+command+="helm repo add molgenis https://helm.molgenis.org && "
+command+="helm repo add molgenis-helm https://registry.molgenis.org/repository/helm && "
+command+="helm repo add elastic https://helm.elastic.co/ && "
+command+="helm repo add fusionauth https://fusionauth.github.io/charts && "
+command+="helm repo add bitnami https://charts.bitnami.com/bitnami && "
+command+="helm repo add jenkins https://charts.jenkins.io && "
+command+="ct lint --all --validate-maintainers=false"
+
+docker run -it --rm --name ct --volume $(pwd):/data quay.io/helmpack/chart-testing:v2.4.0 sh -c "${command}"
