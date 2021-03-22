11d735af50b6dc2bc05376bd3e6722c89255ee42
Fleur Kelpin <f.kelpin@umcg.nl>
2021-03-22 15:58:15 +0100

    chore: upgrade ct version

--- ct.sh
+++ ct.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 command="cd /data && helm init --client-only --stable-repo-url https://charts.helm.sh/stable && ct lint --config ct.yaml --all"
 
-docker run -it --rm --name ct --volume $(pwd):/data quay.io/helmpack/chart-testing:v2.2.0 sh -c "${command}"
+docker run -it --rm --name ct --volume $(pwd):/data quay.io/helmpack/chart-testing:v2.4.1 sh -c "${command}"
